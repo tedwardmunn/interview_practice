9b185cb8c612edc6022d6454749cde2392b5ea5d
Ted Munn <tmunn@candeal.com>
2021-11-28 17:48:04 -0500

    finished string list

--- python/string_list.py
+++ python/string_list.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import random
 
 class StringSet:
 
@@ -23,8 +23,8 @@ class StringSet:
 
     def add(self, str_to_add):
         map_len = len(self.index_nums)
-        self.index_nums[map_len + 1] = str_to_add
-        self.index_strs[str_to_add] = map_len + 1
+        self.index_nums[map_len] = str_to_add
+        self.index_strs[str_to_add] = map_len
 
         print("added")
         print(self.index_nums)
@@ -35,19 +35,51 @@ class StringSet:
         # pop end
         map_len = len(self.index_nums)
         delete_index = self.index_strs[str_to_delete]
-        end_str = self.index_nums[map_len]
+        end_str = self.index_nums[map_len-1]
 
+        if delete_index == map_len:
+            self.index_nums.pop(delete_index)
+            self.index_strs.pop(str_to_delete)
         # remove the str to delete
-        self.index_nums.pop(map_len)
-        self.index_strs.pop(str_to_delete)
+        else:
+            self.index_nums.pop(map_len-1)
+            self.index_strs.pop(str_to_delete)
 
-        # change position of end str
-        self.index_strs[end_str] = delete_index
-        self.index_nums[delete_index] = end_str
+            # change position of end str
+            self.index_strs[end_str] = delete_index
+            self.index_nums[delete_index] = end_str
 
         print("deleted")
         print(self.index_nums)
-        print(self.index_strs
+        print(self.index_strs)
 
-if __name__ == "__main__":
+    
+    def check_if_exists(self, check_str):
+        if check_str in self.index_strs:
+            print(check_str + " is in dict")
+            return True
+        else:
+            print(check_str + " is not in dict")
+            return False
+
+    def uniform_random_sample(self):
+        rand = random.randint(0, len(self.index_nums)-1) 
+        print("random sample: " + self.index_nums[rand])
+        return self.index_nums[rand]
+        
+
+
+
+if __name__ == '__main__':
     test=StringSet(strings=['monday', 'tuesday', 'wednesday'])
+    test.add('thursday')
+    test.check_if_exists('thursday')
+    test.uniform_random_sample()
+    test.uniform_random_sample()
+    test.uniform_random_sample()
+    test.delete('thursday')
+    test.check_if_exists('thursday')
+    test.uniform_random_sample()
+    test.uniform_random_sample()
+    test.uniform_random_sample()
+    # print("test")
